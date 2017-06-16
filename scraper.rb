d4858ba30b38b7bda2224b3b24c286b9c64e093d
Tony Bowden <tony@tmtm.com>
2017-06-16 08:39:21 +0100

    Also scrape 2014-2016 term + Wikidata P39s

--- scraper.rb
+++ scraper.rb
@@ -2,5 +2,10 @@
 # encoding: utf-8
 
 require 'wikidata/fetcher'
-names = WikiData::Category.new( 'Категорија:Пратенички состав 2011-2014', 'mk').member_titles
-EveryPolitician::Wikidata.scrape_wikidata(names: { mk: names }, output: false)
+names = WikiData::Category.new( 'Категорија:Пратенички состав 2011-2014', 'mk').member_titles |
+        WikiData::Category.new( 'Категорија:Пратенички состав 2014-2016', 'mk').member_titles
+
+sparq = 'SELECT ?item WHERE { ?item wdt:P39 wd:Q20015949 . }'
+ids = EveryPolitician::Wikidata.sparql(sparq)
+
+EveryPolitician::Wikidata.scrape_wikidata(ids: ids, names: { mk: names })
