b3408c45cedba97d34f63d973ac5f6083be8175e
Tony Bowden <tony@tmtm.com>
2018-09-10 07:16:01 +0100

    Also get from Albanian Wikipedia
    This seems to have a more complete set of members.

--- scraper.rb
+++ scraper.rb
@@ -2,10 +2,12 @@
 # encoding: utf-8
 
 require 'wikidata/fetcher'
-names = WikiData::Category.new( 'Категорија:Пратенички состав 2011-2014', 'mk').member_titles |
-        WikiData::Category.new( 'Категорија:Пратенички состав 2014-2016', 'mk').member_titles
+mk_names = WikiData::Category.new( 'Категорија:Пратенички состав 2011-2014', 'mk').member_titles |
+           WikiData::Category.new( 'Категорија:Пратенички состав 2014-2016', 'mk').member_titles
+
+sq_names = WikiData::Category.new( 'Kategoria:Anëtarë të Kuvendit të Republikës së Maqedonisë', 'sq').member_titles
 
 sparq = 'SELECT ?item WHERE { ?item wdt:P39 wd:Q20015949 . }'
 ids = EveryPolitician::Wikidata.sparql(sparq)
 
-EveryPolitician::Wikidata.scrape_wikidata(ids: ids, names: { mk: names })
+EveryPolitician::Wikidata.scrape_wikidata(ids: ids, names: { mk: mk_names, sq: sq_names })
